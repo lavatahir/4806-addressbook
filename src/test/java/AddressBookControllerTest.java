f510311f9214cc36208dc3563976be7c3798fd61
lavatahir <lavatahir@cmail.carleton.ca>
2018-02-08 12:57:26 -0500

    added tests

--- src/test/java/AddressBookControllerTest.java
+++ src/test/java/AddressBookControllerTest.java
@@ -1,5 +1,4 @@
 import lab.AddressBook;
-import lab.BuddyInfo;
 import lab.SpringBootApp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,10 +7,14 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringBootApp.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,6 +30,7 @@ public class AddressBookControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+
     @Test
     public void getInitialEmptyAddressBookTest() throws Exception {
         AddressBook expected = new AddressBook();
@@ -36,11 +40,55 @@ public class AddressBookControllerTest {
 
     @Test
     public void addBuddyToBookTest() throws Exception {
-        //this.mockMvc.perform(post(/))
+        String buddyCreate = "{ \"name\": \"Lava\", \"phoneNumber\": \"613-555-5555\" }";
+        String addBuddyToBook = "/addBuddyInfo?bookId=1&buddyId=5";
+        String getAllBooks = "/getAllBooks";
+        this.mockMvc.perform(post("/buddyinfos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(buddyCreate))
+                .andExpect(status()
+                        .isCreated());
+        this.mockMvc.perform(post("/addressbooks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("{}"))
+                .andExpect(status()
+                        .isCreated());
+        this.mockMvc.perform(post(addBuddyToBook))
+                    .andExpect(status().isOk());
+        this.mockMvc.perform(get(getAllBooks))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .string(containsString("Lava")));
     }
 
     @Test
     public void removeBuddyFromBookTest() throws Exception {
+        String buddyCreate = "{ \"name\": \"Lava\", \"phoneNumber\": \"613-555-5555\" }";
+        String addBuddyToBook = "/addBuddyInfo?bookId=1&buddyId=5";
+        String removeBuddyToBook = "/removeBuddyInfo?bookId=1&buddyId=5";
+        String getAllBooks = "/getAllBooks";
 
+        this.mockMvc.perform(post("/buddyinfos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(buddyCreate))
+                .andExpect(status()
+                        .isCreated());
+        this.mockMvc.perform(post("/addressbooks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("{}"))
+                .andExpect(status()
+                        .isCreated());
+        this.mockMvc.perform(post(addBuddyToBook))
+                .andExpect(status().isOk());
+        this.mockMvc.perform(post(removeBuddyToBook))
+                .andExpect(status().isOk());
+        this.mockMvc.perform(get(getAllBooks))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .string(containsString("")));
     }
 }
--- src/test/java/BuddyInfoControllerTest.java
+++ src/test/java/BuddyInfoControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import javax.xml.ws.Response;
 
@@ -15,7 +17,11 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringBootApp.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class BuddyInfoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
     @LocalServerPort
     private int port;
 
